abec1a082f7f670e9b503f6fc3b8c9221f14ec0f
RISHAV KISHORE <rishavkishore732@gmail.com>
2023-12-03 16:49:38 +0530

    It is an API which based CRUD operation by using the tools of node js ,expressjs ,mongodb etc

---
+++ models/course.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+
+
+
+const Course=mongoose.Schema({
+    title:{
+        type:String,
+        require: true,
+    },
+    content:{
+        type:String,
+        require: true
+    },
+    videos:{
+        type:Number,
+        require:true,
+    },
+    active:Boolean,      
+});
+
+module.exports=mongoose.model("courses",Course)
